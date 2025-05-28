c683ec96a4b17e5c8fa057bb1b2b51fda46ec8e3
LuoRongLi <2541077720@qq.com>
2025-05-28 22:51:51 +0800

    feat(RightView): 绑定变量

--- src/views/Login/components/RightView.vue
+++ src/views/Login/components/RightView.vue
@@ -1,22 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useRuleFormRef, useSkip } from '@/hooks'
 
 const formData = ref({
   account: '',
   password: '',
+})
+const {router} = useSkip()
+const {ruleFormRef,submitForm,resetForm} = useRuleFormRef({
+  submit:()=>{
+    router.replace('/')
+  },
 })
 
 </script>
 
 <template>
   <el-card class="w-1/3">
-    Right，Here is a form
-    <el-form>
+    <el-form ref="ruleFormRef" @reset.prevent="resetForm(false)" @submit.prevent="submitForm" :model="formData" >
       <el-form-item label="账号">
-        <el-input placeholder="请输入用户名或者手机号"></el-input>
+        <el-input v-model="formData.account" placeholder="请输入用户名或者手机号"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-input type="password" placeholder="请输入密码"></el-input>
+        <el-input v-model="formData.password" type="password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button native-type="submit" type="primary">登录</el-button>
       </el-form-item>
     </el-form>
   </el-card>
