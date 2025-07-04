a6a4d11c6acb0650f65fe348ed33c543e49a492d
LuoRongLi <2541077720@qq.com>
2025-07-04 23:07:24 +0800

    style:test

--- type/router.d.ts
+++ type/router.d.ts
@@ -12,6 +12,7 @@ declare module 'vue-router' {
     icon?: string[]
     /**
      * 标题
+     *
      *  */
     title?: string
     /**
@@ -28,17 +29,19 @@ declare module 'vue-router' {
      * */
     activePath?: string
     /** 页面加载动画（两种模式，第二种权重更高，第一种直接采用`vue`内置的`transitions`动画，第二种是使用`animate.css`编写进、离场动画，平台更推荐使用第二种模式，已经内置了`animate.css`，直接写对应的动画名即可）`可选` */
-    transition?: {
-      /**
-       * @description 当前路由动画效果
-       * @see {@link https://next.router.vuejs.org/guide/advanced/transitions.html#transitions}
-       * @see animate.css {@link https://animate.style}
-       */
-      name?: string;
-      /** 进场动画 */
-      enterTransition?: string;
-      /** 离场动画 */
-      leaveTransition?: string;
-    }|string;
+    transition?:
+      | {
+          /**
+           * @description 当前路由动画效果
+           * @see {@link https://next.router.vuejs.org/guide/advanced/transitions.html#transitions}
+           * @see animate.css {@link https://animate.style}
+           */
+          name?: string
+          /** 进场动画 */
+          enterTransition?: string
+          /** 离场动画 */
+          leaveTransition?: string
+        }
+      | string
   }
 }
