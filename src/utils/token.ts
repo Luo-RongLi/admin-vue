ddc003c261dac5fa815c98d639c8a26ae1dc696f
luorongli <2541077720@qq.com>
2025-07-11 14:50:27 +0800

    build(vite.config): 更改vite

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,9 +1,8 @@
 import router from '@/router'
 import Cookies from 'js-cookie'
 
-const EXPIRATION: number = 60 * 1000 * 24 * 3
-
-export function setToken(token: string) {
+// 默认三天
+export function setToken(token: string,EXPIRATION: number=60 * 1000 * 24 * 3) {
   // localStorage.setItem('token', token)
   Cookies.set('token', token, { expires: EXPIRATION })
 }
