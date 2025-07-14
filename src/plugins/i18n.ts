acbd979d5b17830ac94a22eed36492f7d6781f99
LuoRongLi <2541077720@qq.com>
2025-07-14 22:13:24 +0800

    feat(i18n): 改改国际化

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,13 +1,14 @@
 import { createI18n } from 'vue-i18n'
-import EN from '@/locales/en-US/en.json'
-import CN from '@/locales/ZH-CN/cn.json'
+import en from '@/locales/en-US/en.json'
+import cn from '@/locales/ZH-CN/cn.json'
 
 const message = {
-  cn:CN,
-  en: EN,
+  cn:cn,
+  en: en,
 }
 
-const i18n = createI18n({
+const i18n = createI18n<[typeof en], 'en' | 'cn'>({
+
   locale: 'cn', // 设置语言类型
   globalInjection: true, // 全局注册$t方法
   messages: message,
--- src/views/Login/components/RightView.vue
+++ src/views/Login/components/RightView.vue
@@ -3,27 +3,26 @@ import { ref } from 'vue'
 import { useRuleFormRef, useSkip } from '@/hooks'
 import { setToken } from '@/utils/token.ts'
 import { useI18n } from 'vue-i18n'
-const t = useI18n()
+const { t } = useI18n()
 type formDataType = {
-  account: string;
-  password: string;
+  account: string
+  password: string
 }
 const formData = ref<formDataType>({
   account: 'admin',
   password: 'admin',
 })
-const {router} = useSkip()
-const {ruleFormRef,submitForm,resetForm,rules} = useRuleFormRef<formDataType>({
-  submit:()=>{
+const { router } = useSkip()
+const { ruleFormRef, submitForm, resetForm, rules } = useRuleFormRef<formDataType>({
+  submit: () => {
     setToken('登录')
     router.replace('/')
   },
-  exRules:{
-    account:{type:'string',message:'请输入账号！！!',trigger:['blur','change']},
-    password:{type:'string',message:'请输入密码！！!',trigger:['blur','change']},
-  }
+  exRules: {
+    account: { type: 'string', message: '请输入账号！！!', trigger: ['blur', 'change'] },
+    password: { type: 'string', message: '请输入密码！！!', trigger: ['blur', 'change'] },
+  },
 })
-
 </script>
 
 <template>
@@ -32,7 +31,14 @@ const {ruleFormRef,submitForm,resetForm,rules} = useRuleFormRef<formDataType>({
       <h1 class="text-2xl">登录</h1>
       <el-text>输入您的邮箱和密码来登录您的账户</el-text>
     </div>
-    <el-form ref="ruleFormRef" label-position="top" :rules="rules" @reset.prevent="resetForm(false)" @submit.prevent="submitForm" :model="formData" >
+    <el-form
+      ref="ruleFormRef"
+      label-position="top"
+      :rules="rules"
+      @reset.prevent="resetForm(false)"
+      @submit.prevent="submitForm"
+      :model="formData"
+    >
       <el-form-item label="账号" prop="account">
         <el-input v-model="formData.account" placeholder="请输入用户名或者手机号"></el-input>
       </el-form-item>
@@ -40,7 +46,9 @@ const {ruleFormRef,submitForm,resetForm,rules} = useRuleFormRef<formDataType>({
         <el-input v-model="formData.password" type="password" placeholder="请输入密码"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button class="mx-auto w-full" native-type="submit" type="primary">{{t('app.login')}}</el-button>
+        <el-button class="mx-auto w-full" native-type="submit" type="primary">{{
+          t('app.login')
+        }}</el-button>
       </el-form-item>
     </el-form>
     <div class="text-center">
@@ -49,6 +57,4 @@ const {ruleFormRef,submitForm,resetForm,rules} = useRuleFormRef<formDataType>({
   </el-card>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
